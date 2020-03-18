922c9cb165767f1d95db58014800616a74078318
Kuba Gerlee_stacjonarny <muczos94@gmail.com>
2020-03-18 16:40:15 +0100

    Init project and create small todo list.

---
+++ src/Components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Checkbox(){
+    return (
+        <div className="todo-item">
+            <input className="checkbox" type="checkbox"/>
+            <p>Placeholder text here</p>
+        </div>
+    )
+}
+
+export default Checkbox
